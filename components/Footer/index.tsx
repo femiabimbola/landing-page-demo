5f5bee4d4f8bd8a66d7bf38a1dc0cc3d30547d34
femi abimbola <femiabimbola6@gmail.com>
2024-12-31 01:41:02 -0800

    changing the component

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -2,16 +2,34 @@ import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 
+type FooterColumnProps = {
+  title: string;
+  children: React.ReactNode;
+}
+
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      {children}
+    </div>
+  )
+}
+
 const Footer = () => {
   return (
-    <footer className="flexCenter mb-4">
-      <div className="padding-container max-container flex w-full flex-col gap-10">
-        <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
-          <Link href="/" className="mb-5">
-            <Image src="/assets/hilink-logo.svg" alt="logo" width={74} height={29}/>
-          </Link>
+    <footer className="flex items-center justify-center mb-4 ">
+      <div className="px-6 lg:px-20 3xl:px-0 mx-auto max-w-[1440px] flex w-full flex-col gap-10 ">
+        <div className="flex flex-col items-start justify-center gap-[5%] md:flex-row">
 
-          <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
+          {/* The logo */}
+          <Link href="/" className="mb-5 ">
+            <Image src="/assets/hilink-logo.svg" alt="logo" width={74} height={29}/>
+            <p> Add some tagline to the logo </p>
+          </Link>
+          {/* The end of logo  */}
+
+          <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1 border border-blue-700'> 
             {FOOTER_LINKS.map((columns) => (
               <FooterColumn title={columns.title}>
                 <ul className="regular-14 flex flex-col gap-4 text-gray-50">
@@ -64,18 +82,6 @@ const Footer = () => {
   )
 }
 
-type FooterColumnProps = {
-  title: string;
-  children: React.ReactNode;
-}
 
-const FooterColumn = ({ title, children }: FooterColumnProps) => {
-  return (
-    <div className="flex flex-col gap-5">
-      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
-      {children}
-    </div>
-  )
-}
 
 export default Footer
